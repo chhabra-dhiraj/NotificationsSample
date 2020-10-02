10ce7ad2a78701fddd53a5cb448c720626671bee
Dhiraj Chhabra <dhirajchhabra630@gmail.com>
2020-10-02 12:58:38 +0530

    added Http logging interceptor and resolved connection timeout bug

--- app/src/main/java/io/github/chhabra_dhiraj/notire/network/Network.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/notire/network/Network.kt
@@ -1,9 +1,25 @@
 package io.github.chhabra_dhiraj.notire.network
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object Network {
+
+    private val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+        redactHeader("Authorization")
+        redactHeader("Cookie")
+    }
+
+    private val okHttpClient = OkHttpClient.Builder().apply {
+        readTimeout(300, TimeUnit.SECONDS)
+        connectTimeout(300, TimeUnit.SECONDS)
+        writeTimeout(300, TimeUnit.SECONDS)
+        addInterceptor(httpLoggingInterceptor)
+    }.build()
 
     // Configure retrofit to parse JSON and use coroutines
     private val retrofit = Retrofit.Builder()
